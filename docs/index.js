8d5862d3b884bbeae00fdd41d95b69e08ded5ba4
Tim de Koning <git@king-management.nl>
2023-03-28 22:06:23 +0200

    Disable cargo.site autosizing

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,6 @@
-console.log("test");
+// Stop cargo.site auto resizing logic
+window.baseUnit.stopListening();
+window.baseUnit.removeEventListeners();
+
+// Remove any configure autosizing font size
+document.documentElement.removeAttribute("style");
