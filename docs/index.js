3a3f40ca6eaf3b92a503fa5c9e29bfdf6470eb80
Tim de Koning <git@king-management.nl>
2023-03-28 22:47:20 +0200

    Allow scrolling to anchors

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,26 @@
-// Stop cargo.site auto resizing logic
+function scrollToHash() {
+  const pageDiv = document.getElementById(window.location.hash.substr(1));
+  const scrollingContainer = document.querySelector(
+    ".content_container bodycopy.content_padding .page_content"
+  );
+  if (!pageDiv || !scrollingContainer) {
+    return;
+  }
+  scrollingContainer.scrollTo({
+    left: pageDiv.getBoundingClientRect().x + scrollingContainer.scrollLeft - 100,
+    behavior: "smooth",
+  });
+}
+
 window.addEventListener("load", () => {
+  // Stop cargo.site auto resizing logic
   window.baseUnit.stopListening();
   window.baseUnit.removeEventListeners();
 
   // Remove any configure autosizing font size
   document.documentElement.removeAttribute("style");
+
+  // Install scroll handlers
+  window.addEventListener("hashchange", scrollToHash);
+  scrollToHash();
 });
