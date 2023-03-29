eba25b53b314c9e651200ac516904e9375874aa2
Tim de Koning <git@king-management.nl>
2023-03-29 15:19:27 +0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -12,6 +12,9 @@ function scrollToHash() {
   });
 }
 
+window.BaseUnit.prototype.initialize = function () { console.log('initialize overloaded'); }
+window.BaseUnit.prototype.onDocumentReady = function () { console.log('onDocumentReady overloaded'); }
+
 window.addEventListener("load", () => {
   // Stop cargo.site auto resizing logic
   window.baseUnit.stopListening();
